04546c05b125800aa8d447a719d27b20e2087316
Gert Vermeer <gert@vermeernet.nl>
2020-05-13 16:38:34 +0200

    min temp ook

--- sunny.py
+++ sunny.py
@@ -33,13 +33,11 @@ def home():
 @app.route('/maxtemp', methods=['GET'])
 def maxtemp():
     max_url= 'http://ledmachine.local:8086/query?db=weerstats&q=SELECT max(buiten)  from temperatuur where time >= \'from_time\' and time < \'to_time\''
-    resp  = requests.get('http://ledmachine.local:8086/query?db=weerstats&q=SELECT top(max,10) FROM (SELECT max(buiten) from temperatuur GROUP BY time(24h))', verify=False)
+    max_today_url = 'http://ledmachine.local:8086/query?db=weerstats&q=SELECT max(buiten) from temperatuur where time > now() - 24h'
+    resp  = requests.get('http://ledmachine.local:8086/query?db=weerstats&q=SELECT top(max,5) FROM (SELECT max(buiten) from temperatuur GROUP BY time(24h))', verify=False)
     json_data = resp.json()
 
     date =  json_data.get("results",{})[0].get("series")[0].get("values")[0][0]
-
-
-
     testdata = json_data.get("results",{})[0].get("series")[0].get("values")
     print testdata
     print "************"
@@ -55,9 +53,36 @@ def maxtemp():
         print result
 
 
+    response = requests.get(max_today_url).json()
+    result = result + str(response.get("results",{})[0].get("series")[0].get("values")[0][1]) + " C     " +return_data(response.get("results",{})[0].get("series")[0].get("values")[0][0])  + " ;"
     return jsonify(result)
 
+@app.route('/mintemp', methods=['GET'])
+def mintemp():
+    max_url= 'http://ledmachine.local:8086/query?db=weerstats&q=SELECT min(buiten)  from temperatuur where time >= \'from_time\' and time < \'to_time\''
+    max_today_url = 'http://ledmachine.local:8086/query?db=weerstats&q=SELECT min(buiten) from temperatuur where time > now() - 24h'
+    resp  = requests.get('http://ledmachine.local:8086/query?db=weerstats&q=SELECT bottom(min,5) FROM (SELECT min(buiten) from temperatuur GROUP BY time(24h))', verify=False)
+    json_data = resp.json()
 
+    date =  json_data.get("results",{})[0].get("series")[0].get("values")[0][0]
+    testdata = json_data.get("results",{})[0].get("series")[0].get("values")
+    print testdata
+    print "************"
+    testdata = sorted(testdata, key=itemgetter(1), reverse=True)
+    result = ""
+    for item in testdata:
+        print return_sql_date(item[0]) #  SELECT max(buiten)  from temperatuur where time >= '2020-05-09 00:00:00' and time < '2020-06-10 00:00:00'
+        tempurl = re.sub("from_time",return_sql_date(item[0])+ " 00:00:00",max_url)
+        tempurl = re.sub("to_time",return_sql_date(item[0])+ " 23:59:59",tempurl)
+        print tempurl
+        response = requests.get(tempurl).json()
+        result = result + str(response.get("results",{})[0].get("series")[0].get("values")[0][1]) + " C     " +return_data(response.get("results",{})[0].get("series")[0].get("values")[0][0])  + " ;"
+        print result
+
+
+    response = requests.get(max_today_url).json()
+    result = result + str(response.get("results",{})[0].get("series")[0].get("values")[0][1]) + " C     " +return_data(response.get("results",{})[0].get("series")[0].get("values")[0][0])  + " ;"
+    return jsonify(result)
 
 app.run(host= '0.0.0.0', port='3001')
 
